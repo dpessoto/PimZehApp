1d06cdedf1ad235d1bc4c2667f0ba8213b2cf93a
daniel.pessoto <daniel.pessoto@valemobi.com.br>
2020-09-27 13:55:22 -0300

    Adicionando classe Usuario.java

---
+++ app/src/main/java/com/unip/pimzeh/model/Usuario.java
@@ -0,0 +1,44 @@
+package com.unip.pimzeh.model;
+
+public class Usuario {
+
+    private int cpf;
+    private String nome;
+    private String email;
+    private String senha;
+
+    public Usuario() {
+    }
+
+    public int getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(int cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+}
