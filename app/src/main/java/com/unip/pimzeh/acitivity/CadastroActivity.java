0426c1644d46944e49b4a4e0508e7fdcf507ab59
daniel.pessoto <daniel.pessoto@valemobi.com.br>
2020-09-27 13:36:10 -0300

    Adicionando tela de login e cadastro

---
+++ app/src/main/java/com/unip/pimzeh/acitivity/CadastroActivity.java
@@ -0,0 +1,16 @@
+package com.unip.pimzeh.acitivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.unip.pimzeh.R;
+
+public class CadastroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro);
+    }
+}
---
+++ app/src/main/java/com/unip/pimzeh/acitivity/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.unip.pimzeh.acitivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.unip.pimzeh.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
